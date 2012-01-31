d53b02c9f5d48da1ba4efe093a4f88177c8a8313
Marco Antonio Quispe Perez <marco.quispe.perez@gmail.com>
2012-01-30 21:43:58 -0500

    primer chat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = 'bb08ee54fe5b884371bb040afcea18243f76ddd5a5675f2f830679089e91d41e3b1862d0a0f72f2cb893f3fcc975c58b0b553c30f0de408885bb255496eb541b'
